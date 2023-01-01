95cf97838f7bcbca908af4d0062f0bf3f905e5ae
Sailesh Dahal <saileshbro@gmail.com>
2023-01-01 10:20:26 +0545

    test: update `NetworkFailure` tests

--- failures/test/src/network_failure/network_failure_test.dart
+++ failures/test/src/network_failure/network_failure_test.dart
@@ -1,4 +1,4 @@
-import 'package:failures/src/network_failure/network_failure.dart';
+import 'package:failures/failures.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -6,24 +6,24 @@ void main() {
     test('fromJson Test', () {
       final json = {
         'message': 'message',
-        'code': 1,
+        'status_code': 1,
         'errors': ['error1', 'error2'],
       };
       final networkFailure = NetworkFailure.fromJson(json);
       expect(networkFailure.message, 'message');
-      expect(networkFailure.code, 1);
+      expect(networkFailure.statusCode, 1);
       expect(networkFailure.errors, orderedEquals(['error1', 'error2']));
     });
 
     test('toJson Test', () {
       const networkFailure = NetworkFailure(
         message: 'message',
-        code: 1,
+        statusCode: 1,
         errors: ['error1', 'error2'],
       );
       final json = networkFailure.toJson();
       expect(json['message'], 'message');
-      expect(json['code'], 1);
+      expect(json['status_code'], 1);
       expect(json['errors'], orderedEquals(['error1', 'error2']));
     });
   });
