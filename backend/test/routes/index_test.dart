2a71820d15d20d97f81b6db43c962cd3e8bcb85b
Sailesh Dahal <saileshbro@gmail.com>
2023-01-01 15:25:43 +0545

    test: update `/` route test

--- backend/test/routes/index_test.dart
+++ backend/test/routes/index_test.dart
@@ -10,14 +10,10 @@ class _MockRequestContext extends Mock implements RequestContext {}
 
 void main() {
   group('GET /', () {
-    test('responds with a 200 and "Welcome to Dart Frog!".', () async {
+    test('responds with a 405', () async {
       final context = _MockRequestContext();
       final response = await route.onRequest(context);
-      expect(response.statusCode, equals(HttpStatus.ok));
-      expect(
-        response.body(),
-        completion(equals('Welcome to Dart Frog!')),
-      );
+      expect(response.statusCode, equals(HttpStatus.methodNotAllowed));
     });
   });
 }
