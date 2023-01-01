cc4c0353529759eed53a90bc071155756b953c67
Sailesh Dahal <saileshbro@gmail.com>
2023-01-01 15:18:55 +0545

    refactor: update `/` route as not allowed

--- backend/routes/index.dart
+++ backend/routes/index.dart
@@ -1,11 +1,10 @@
-import 'package:backend/db/database_connection.dart';
+import 'dart:io';
+
 import 'package:dart_frog/dart_frog.dart';
 
 Future<Response> onRequest(RequestContext context) async {
-  final connection = context.read<DatabaseConnection>();
-  await connection.connect();
-  final response =
-      await connection.db.query('select * from information_schema.tables');
-  await connection.close();
-  return Response.json(body: response.map((e) => e.toColumnMap()).toList());
+  return Response.json(
+    body: {'error': '👀 Looks like you are lost 🔦'},
+    statusCode: HttpStatus.methodNotAllowed,
+  );
 }
