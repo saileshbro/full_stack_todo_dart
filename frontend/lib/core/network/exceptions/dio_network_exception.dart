bee807f59d1ec4b95a889fce2e660d9be2b76e58
Sailesh Dahal <saileshbro@gmail.com>
2023-01-12 13:32:16 +0545

    feat: create `DioNetworkException`

---
+++ frontend/lib/core/network/exceptions/dio_network_exception.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:exceptions/exceptions.dart';
+
+class DioNetworkException extends DioError implements NetworkException {
+  DioNetworkException({
+    required this.message,
+    required this.statusCode,
+    required this.errors,
+    required super.requestOptions,
+  });
+
+  @override
+  final int statusCode;
+  @override
+  final String message;
+  @override
+  final Map<String, List<String>> errors;
+}
