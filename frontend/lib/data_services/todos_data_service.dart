53707ed1142af375434f6bccbc6cf429ae980f3a
Sailesh Dahal <saileshbro@gmail.com>
2023-01-12 15:57:45 +0545

    feat: add `todos_data_service`

---
+++ frontend/lib/data_services/todos_data_service.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
+import 'package:models/models.dart';
+import 'package:stacked/stacked.dart';
+
+@lazySingleton
+class TodosDataService with ReactiveServiceMixin {
+  TodosDataService() {
+    listenToReactiveValues([_todos]);
+  }
+  late final ReactiveValue<List<Todo>> _todos = ReactiveValue<List<Todo>>([]);
+
+  List<Todo> get todos => _todos.value;
+
+  @visibleForTesting
+  Stream<Iterable<Todo>> get todosStream => _todos.values;
+
+  void add(Todo todo) {
+    final index = _todos.value.indexWhere((element) => element.id == todo.id);
+    if (index == -1) {
+      _todos.value.insert(0, todo);
+    } else {
+      _todos.value[index] = todo;
+    }
+    notifyListeners();
+  }
+
+  void addAll(List<Todo> todos) {
+    _todos.value
+      ..clear()
+      ..insertAll(0, todos);
+    notifyListeners();
+  }
+
+  void remove(Todo todo) {
+    _todos.value.removeWhere((element) => element.id == todo.id);
+    notifyListeners();
+  }
+}
---
+++ frontend/test/data_services/todos_data_service_test.dart
@@ -0,0 +1,65 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fullstack_todo/data_services/todos_data_service.dart';
+import 'package:models/models.dart';
+
+void main() {
+  late TodosDataService sut;
+  setUp(() {
+    sut = TodosDataService();
+  });
+  group('TodosDataServiceTest -', () {
+    test('operations test', () {
+      expect(sut.todos, equals([]));
+      sut.add(_todo1);
+      expect(sut.todos, orderedEquals([_todo1]));
+      sut.add(_todo2);
+      expect(sut.todos, orderedEquals([_todo2, _todo1]));
+      sut.remove(_todo1);
+      expect(sut.todos, orderedEquals([_todo2]));
+      sut.remove(_todo2);
+      expect(sut.todos, equals([]));
+      sut.addAll(_todos);
+      expect(sut.todos, orderedEquals(_todos));
+      final updated = _todo1.copyWith(
+        completed: true,
+        updatedAt: DateTime.now(),
+      );
+      sut.add(updated);
+      expect(sut.todos, orderedEquals([updated, _todo2]));
+    });
+    test('reactivityTest', () async {
+      sut.add(_todo1);
+      expect(await sut.todosStream.first, equals([_todo1]));
+      sut.add(_todo2);
+      expect(await sut.todosStream.first, equals([_todo2, _todo1]));
+      sut.remove(_todo1);
+      expect(await sut.todosStream.first, equals([_todo2]));
+      final updated = _todo2.copyWith(
+        completed: true,
+        updatedAt: DateTime.now(),
+      );
+      sut.add(updated);
+      expect(await sut.todosStream.first, equals([updated]));
+      sut.remove(_todo2);
+      expect(await sut.todosStream.first, equals([]));
+    });
+  });
+}
+
+final _todo1 = Todo(
+  id: 1,
+  title: 'title1',
+  description: 'description1',
+  completed: false,
+  createdAt: DateTime.now(),
+);
+
+final _todo2 = Todo(
+  id: 2,
+  title: 'title2',
+  description: 'description2',
+  completed: false,
+  createdAt: DateTime.now(),
+);
+
+final _todos = [_todo1, _todo2];
