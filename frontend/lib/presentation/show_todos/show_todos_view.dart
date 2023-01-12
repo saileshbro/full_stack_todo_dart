31b0aa3cdf1e0df366a7b88e4a790850d63f898a
Sailesh Dahal <saileshbro@gmail.com>
2023-01-12 16:40:50 +0545

    feat: implement `TodoListTile` widget

--- frontend/lib/presentation/show_todos/show_todos_view.dart
+++ frontend/lib/presentation/show_todos/show_todos_view.dart
@@ -1,26 +1,65 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:fullstack_todo/core/di/locator.dart';
 import 'package:fullstack_todo/presentation/show_todos/show_todos_viewmodel.dart';
-
+import 'package:fullstack_todo/presentation/show_todos/widgets/todo_list_tile.dart';
 import 'package:stacked/stacked.dart';
 
 class ShowTodosView extends StatelessWidget {
-  const ShowTodosView({Key? key}) : super(key: key);
+  const ShowTodosView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ViewModelBuilder<ShowTodosViewModel>.nonReactive(
-      viewModelBuilder: () => locator<ShowTodosViewModel>(),
+    return ViewModelBuilder<ShowTodosViewModel>.reactive(
+      viewModelBuilder: locator,
+      onModelReady: (model) => model.init(),
       builder: (
         BuildContext context,
         ShowTodosViewModel model,
         Widget? child,
       ) {
         return Scaffold(
-          body: Center(
-            child: Text(
-              'ShowTodosView',
-            ),
+          appBar: AppBar(
+            title: const Text('Todos'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: model.handleTodo,
+            child: const Icon(Icons.add),
+          ),
+          body: Builder(
+            builder: (context) {
+              if (model.isBusy) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (model.hasError) {
+                return Center(child: Text(model.modelError.toString()));
+              }
+              if (model.todos.isEmpty) {
+                return Center(
+                  child: Text(
+                    'No todos found!',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                );
+              }
+              return RefreshIndicator(
+                onRefresh: model.refresh,
+                child: ScrollConfiguration(
+                  behavior: ScrollConfiguration.of(context).copyWith(
+                    dragDevices: {
+                      PointerDeviceKind.touch,
+                      PointerDeviceKind.mouse,
+                    },
+                  ),
+                  child: ListView.builder(
+                    physics: const AlwaysScrollableScrollPhysics(),
+                    itemCount: model.todos.length,
+                    itemBuilder: (context, index) =>
+                        TodoListTile(todo: model.todos[index]),
+                  ),
+                ),
+              );
+            },
           ),
         );
       },
---
+++ frontend/lib/presentation/show_todos/widgets/todo_list_tile.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:fullstack_todo/presentation/show_todos/show_todos_viewmodel.dart';
+import 'package:models/models.dart';
+import 'package:stacked/stacked.dart';
+
+class TodoListTile extends ViewModelWidget<ShowTodosViewModel> {
+  const TodoListTile({
+    super.key,
+    required this.todo,
+  });
+
+  final Todo todo;
+
+  @override
+  Widget build(BuildContext context, ShowTodosViewModel viewModel) {
+    return ListTile(
+      title: Text(todo.title),
+      subtitle: Text(todo.description),
+      leading: Checkbox(
+        key: Key('todoCheckbox${todo.title}}'),
+        value: todo.completed,
+        onChanged: (val) => viewModel.markCompleted(todo),
+      ),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            icon: const Icon(
+              Icons.edit,
+              color: Colors.blue,
+            ),
+            onPressed: () => viewModel.handleTodo(todo),
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.delete,
+              color: Colors.red,
+            ),
+            onPressed: () => viewModel.deleteTodo(todo),
+          ),
+        ],
+      ),
+    );
+  }
+}
