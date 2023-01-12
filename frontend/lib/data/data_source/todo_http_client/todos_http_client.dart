1f2910cc0f53d95d8d9db227f1dc29f32525259c
Sailesh Dahal <saileshbro@gmail.com>
2023-01-12 13:02:25 +0545

    feat: implement `TodosHttpClient`

---
+++ frontend/lib/data/data_source/todo_http_client/todos_http_client.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:models/models.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:typedefs/typedefs.dart';
+
+part 'todos_http_client.g.dart';
+
+@RestApi()
+@lazySingleton
+abstract class TodosHttpClient {
+  @factoryMethod
+  factory TodosHttpClient(Dio _dio) = _TodosHttpClient;
+
+  @GET('/todos')
+  Future<List<Todo>> getAllTodo();
+  @GET('/todos/{id}')
+  Future<Todo> getTodoById(@Path('id') TodoId id);
+  @POST('/todos')
+  Future<Todo> createTodo(@Body() CreateTodoDto todo);
+  @PATCH('/todos/{id}')
+  Future<Todo> updateTodo(@Path('id') TodoId id, @Body() UpdateTodoDto todo);
+  @DELETE('/todos/{id}')
+  Future<Todo> deleteTodoById(@Path('id') TodoId id);
+}
---
+++ frontend/lib/data/data_source/todo_http_client/todos_http_client.g.dart
@@ -0,0 +1,155 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'todos_http_client.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+
+class _TodosHttpClient implements TodosHttpClient {
+  _TodosHttpClient(
+    this._dio, {
+    this.baseUrl,
+  });
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<List<Todo>> getAllTodo() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final _result =
+        await _dio.fetch<List<dynamic>>(_setStreamType<List<Todo>>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/todos',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    var value = _result.data!
+        .map((dynamic i) => Todo.fromJson(i as Map<String, dynamic>))
+        .toList();
+    return value;
+  }
+
+  @override
+  Future<Todo> getTodoById(id) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final _result =
+        await _dio.fetch<Map<String, dynamic>>(_setStreamType<Todo>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/todos/${id}',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = Todo.fromJson(_result.data!);
+    return value;
+  }
+
+  @override
+  Future<Todo> createTodo(todo) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(todo.toJson());
+    final _result =
+        await _dio.fetch<Map<String, dynamic>>(_setStreamType<Todo>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/todos',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = Todo.fromJson(_result.data!);
+    return value;
+  }
+
+  @override
+  Future<Todo> updateTodo(
+    id,
+    todo,
+  ) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(todo.toJson());
+    final _result =
+        await _dio.fetch<Map<String, dynamic>>(_setStreamType<Todo>(Options(
+      method: 'PATCH',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/todos/${id}',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = Todo.fromJson(_result.data!);
+    return value;
+  }
+
+  @override
+  Future<Todo> deleteTodoById(id) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final _result =
+        await _dio.fetch<Map<String, dynamic>>(_setStreamType<Todo>(Options(
+      method: 'DELETE',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/todos/${id}',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = Todo.fromJson(_result.data!);
+    return value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+}
