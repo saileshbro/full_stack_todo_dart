528358a369aabb4e3efcbcd9ffcf01df115c4623
Sailesh Dahal <saileshbro@gmail.com>
2023-01-12 13:54:03 +0545

    feat: add interceptor to dio obj

--- frontend/lib/core/di/third_party_modules.dart
+++ frontend/lib/core/di/third_party_modules.dart
@@ -1,10 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:fullstack_todo/constants/constants.dart';
+import 'package:fullstack_todo/core/network/interceptors/network_error_interceptor.dart';
 import 'package:injectable/injectable.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 @module
 abstract class ThirdPartyModules {
   @lazySingleton
-  Dio get dio => Dio(BaseOptions(baseUrl: kBaseUrl));
+  Dio get dio => Dio(BaseOptions(baseUrl: kBaseUrl))
+    ..interceptors.add(NetworkErrorInterceptor());
 }
