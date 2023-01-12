e8e2f838b7a312ea1bedf414887152b4932e121b
Sailesh Dahal <saileshbro@gmail.com>
2023-01-12 17:37:11 +0545

    feat: update route

--- frontend/lib/core/app/routes.dart
+++ frontend/lib/core/app/routes.dart
@@ -1,3 +1,4 @@
+import 'package:fullstack_todo/presentation/maintain_todo/maintain_todo_view.dart';
 import 'package:fullstack_todo/presentation/show_todos/show_todos_view.dart';
 import 'package:stacked/stacked_annotations.dart';
 
@@ -6,4 +7,5 @@ import 'package:stacked/stacked_annotations.dart';
 )
 const List<StackedRoute<dynamic>> routes = [
   AdaptiveRoute(page: ShowTodosView, initial: true),
+  AdaptiveRoute(page: MaintainTodoView),
 ];
